f1482b6d4c5ca09bbb90edfdea69d795f71f99c3
TarunSehgal27 <54267786+TarunSehgal27@users.noreply.github.com>
2020-03-11 22:54:45 +0530

    Create que3.java

---
+++ WEEK 06/que3.java
@@ -0,0 +1,12 @@
+interface A {
+	public abstract void run();
+}
+class B implements A {
+	public void run() {}
+}
+class MyThread extends B
+{
+  public void run(){
+    System.out.print("NPTEL Java");
+  }
+}
