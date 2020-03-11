f8948d9b526eed143757b3caa1048a7332376cb6
TarunSehgal27 <54267786+TarunSehgal27@users.noreply.github.com>
2020-03-11 22:51:56 +0530

    Create que1.java

---
+++ WEEK 06/que1.java
@@ -0,0 +1,10 @@
+public class Question61 extends Thread{
+  public void run(){
+    System.out.print("Thread is Running.");
+  }
+  
+public static void main(String args[]){  
+		Question61 thread=new Question61();  
+		thread.start();
+	}  
+}
